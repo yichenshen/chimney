1566c6387a97fd974c589d0f1b115469388a1b85
Shen Yichen <2007.yichen@gmail.com>
2016-02-05 01:34:02 +0800

    Add labels navbar
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #Gets labels for all actions
+  before_action :get_all_labels, if: Proc.new{|c| request.get?}
+  
+  private
+    # Gets labels to show in view
+    def get_all_labels
+      @labels = Label.all
+    end
 end
