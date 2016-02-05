1ee997ec56a79395ec650de0268ad14ff6a1d94e
Shen Yichen <2007.yichen@gmail.com>
2016-02-06 01:50:58 +0800

    Add validation
    For titles of label and todo.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- app/models/errand.rb
+++ app/models/errand.rb
@@ -1,3 +1,4 @@
 class Errand < ActiveRecord::Base
 	has_and_belongs_to_many :labels, inverse_of: :errand
+	validates :title, presence: true, length: {maximum: 100}
 end
--- app/models/label.rb
+++ app/models/label.rb
@@ -1,3 +1,4 @@
 class Label < ActiveRecord::Base
 	has_and_belongs_to_many :errands, inverse_of: :label
+	validates :name, presence: true, uniqueness: true ,length: {maximum: 25}
 end
