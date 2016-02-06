cc9932fcae6e198fd59df8e71b55293b7ef9ffd6
Shen Yichen <2007.yichen@gmail.com>
2016-02-07 02:03:19 +0800

    Fix helper scope
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- app/helpers/errands_helper.rb
+++ app/helpers/errands_helper.rb
@@ -1,12 +1,14 @@
 module ErrandsHelper
-	def ordered_errand_list(collection)
-    	@errands_due = collection.where.not(deadline: nil).order(:deadline, :created_at)
-	    @errands_no_due = collection.where(deadline: nil).order(:created_at)
-	    @errands = @errands_due + @errands_no_due
+	# Gets a list of uncompleted TODOs, ordered by deadline first, then by time created
+    def ordered_errand_list(collection)
+    	errands_due = collection.where(done: false).where.not(deadline: nil).order(:deadline, :created_at)
+	    errands_no_due = collection.where(done: false, deadline: nil).order(:created_at)
+	    
+        errands_due + errands_no_due
 	end
 
 	def set_display_properties(collection)
-		@errands.each do |e|
+		collection.each do |e|
 		  	if !e.content or e.content.empty?
                 e.content = "[No content]"
 			end
