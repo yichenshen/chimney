089f61544b6e6fe1468760c90ed8facdab4f6454
Shen Yichen <2007.yichen@gmail.com>
2016-02-06 18:16:51 +0800

    Add deadline option to form
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,7 +22,7 @@ $( document ).ready(function() {
 	// Initialize collapse button
 	$(".menu-btn").sideNav();
 	// Initialize collapsible
-		$('.collapsible').collapsible({
+	$('.collapsible').collapsible({
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
 });
