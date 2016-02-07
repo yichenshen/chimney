778ed783e6a5fb2be902c25a20e79322cc95d822
Shen Yichen <2007.yichen@gmail.com>
2016-02-07 17:49:23 +0800

    Add session replacement skeleton
    Added UI elements to replace the default session.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,4 +25,5 @@ $( document ).ready(function() {
 	$('.collapsible').collapsible({
       accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
+    $('.modal-trigger').leanModal();
 });
