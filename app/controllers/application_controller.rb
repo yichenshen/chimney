93c4efa85334d66a6461cbee341fec9eaef4d174
Shen Yichen <2007.yichen@gmail.com>
2016-02-07 15:44:00 +0800

    Change routes for shared templates
    Also adjusted route fro application controller. Before action is removed, and should be included in the individual classes instead.
    
    Signed-off-by: Shen Yichen <2007.yichen@gmail.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  #Gets labels for all actions
-  before_action :get_all_labels, if: Proc.new{|c| request.get?}
   
   private
     # Gets labels to show in view
     def get_all_labels
-      @labels = Label.all
+      @labels = @app_session.labels
     end
 end
